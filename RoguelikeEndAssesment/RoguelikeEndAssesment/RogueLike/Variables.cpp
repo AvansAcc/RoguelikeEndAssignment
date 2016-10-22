926d614875117ae76bb302226cce12161b627507
Rick Smeets <Rick@smeets60.nl>
2016-10-22 22:12:22 +0200

    Variables Memory leak gefixed

---
+++ RoguelikeEndAssesment/RoguelikeEndAssesment/RogueLike/Variables.cpp
@@ -0,0 +1,11 @@
+#include "Variables.h"
+
+namespace RogueLike {
+	namespace Globals {
+
+		const bool DEBUG = true;
+		char const * const ROOM_OPTIONS[7] = { "vechten", "vluchten", "oppakken", "uitrusten", "spullen zien", "kaart bekijken", "eigenschappen zien" };
+		char const * const COMBAT_OPTIONS[3] = { "aanvallen", "vluchten", "spullen gebruiken" };
+	
+	}
+}
